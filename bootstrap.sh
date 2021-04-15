ae9790e3d6c8d531124868ae5a76610a94d153b3
Ikhwan Abdullah <mikhwan.abdullah@digi.com.my>
2021-04-15 21:18:53 +0800

    chore: install powerline and fzf, and add vim color

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,38 +1,46 @@
 #!/bin/bash
 
 
-#Requirement:
+# Requirement:
 # Install git beforehand so that we can checkout the dotfiles from repo
 # Installation is assuming that you're using Fedora, because I'm sticking to Fedora
 
 # Checks prerequisite for installation
 
+# install Powerline
+sudo dnf install -y powerline powerline-fonts
+
 # Install Vim-Pathogen
 echo "Intall Vim Pathogen"
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-  curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # Install all vim's plugins that I use.
 echo "Intall My Vim Plugin"
 # Read from vim.plugins file
 for i in $(cat vim.plugins); do
-  cd ~/.vim/bundle/
-  git clone $i
-  cd -
+ cd ~/.vim/bundle/
+ git clone $i
+ cd -
 done
+
+# Create vim colors
+mkdir ~/.vim/colors/
+# Copy my color schemes
+cp ~/dotfiles/vim.colors/* ~/.vim/colors/
 
 echo "Install FiraCode for font Ligatures"
 # Install FiraCode Font Ligatures (docs: https://github.com/tonsky/FiraCode/wiki)
 # This is nice in konsole/terminal, but, Terminator doesnt' support ligatures, so... shucks!
 # create fonts dir
 mkdir -p ~/.local/share/fonts
-#
+
 # run installation
 for type in Bold Light Medium Regular Retina; do
-  wget -O ~/.local/share/fonts/FiraCode-${type}.ttf \
-    "https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-${type}.ttf?raw=true";
+ wget -O ~/.local/share/fonts/FiraCode-${type}.ttf \
+   "https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-${type}.ttf?raw=true";
 done
-#
+
 fc-cache -f
 
 # TODO ::  Install PHP Brew for managing different version of PHP with ease (docs: https://github.com/phpbrew/phpbrew)
@@ -49,13 +57,15 @@ sudo mv phpbrew.phar /usr/local/bin/phpbrew
 phpbrew init
 
 # Install PHP 7.1, and PHP 7.2 for prosperity (Using laravel standard installation)
-phpbrew install 7.1.33 +default +mysql +sqlite +pdo +openssl +mbstring +xml +json -- --with-openssl
-phpbrew install 7.2.31 +default +mysql +sqlite +pdo +openssl +mbstring +xml +json -- --with-openssl
-phpbrew install 7.3 +default +mysql +sqlite +pdo +openssl +mbstring +xml +json -- --with-openssl
+phpbrew install 7.1.33 +default +mysql +sqlite +pdo +openssl +mbstring +xml +json +pgsql +pdo -- --with-openssl
+# phpbrew install 7.2.31 +default +mysql +sqlite +pdo +openssl +mbstring +xml +json -- --with-openssl
+# phpbrew install 7.3 +default +mysql +sqlite +pdo +openssl +mbstring +xml +json -- --with-openssl
 
-# TODO ::  Install Fuzzy Finder
-# TODO ::  Install icons-in-terminal
 # TODO ::  Install keychain?
+
+# Install Fuzzy Finder
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
 
 
 # Create Symlink for the dotfiles to home dir
